240de8cbad5f5b091b7786a17862a6dfd91b247e
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2017-02-03 17:23:10 +0800

    more notes about graph builder

--- src/main/scala/net/zhenglai/akka/quest/stream/SimpleGraphMain.scala
+++ src/main/scala/net/zhenglai/akka/quest/stream/SimpleGraphMain.scala
@@ -33,12 +33,20 @@ object SimpleGraphMain extends App {
   // Such graph is simple to translate to the Graph DSL since each linear element corresponds to a Flow,
   // and each circle corresponds to either a Junction or a Source or Sink if it is beginning or ending a Flow.
   // Junction reference equality defines graph node equality (i.e. the same merge instance used in a GraphDSL refers to the same location in the resulting graph).
+
+  // Once the GraphDSL has been constructed though, the GraphDSL instance is immutable, thread-safe, and freely shareable.
+  // The same is true of all graph pieces—sources, sinks, and flows
   val g = RunnableGraph.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
     import GraphDSL.Implicits._ // bring in ~> & <~
     val in = Source(1 to 10)
     val out = Sink.foreach(println)
 
+    //  builder.add(...), an operation that will make a copy of the blueprint that is passed to it and return the inlets and outlets of the resulting copy so that they can be wired up.
+
+    // import broadcast junction
     val bcast = builder.add(Broadcast[Int](2))
+
+    // import merge junction
     val merge = builder.add(Merge[Int](2))
 
     val f1, f2, f3, f4 = Flow[Int].map(_ + 10)
