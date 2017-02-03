0008733e49a07f85a603d43950d4281ee73c9d42
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2017-02-03 18:16:19 +0800

    partial graph+

--- src/main/scala/net/zhenglai/akka/quest/stream/PartialGraphMain.scala
+++ src/main/scala/net/zhenglai/akka/quest/stream/PartialGraphMain.scala
@@ -3,9 +3,13 @@ package net.zhenglai.akka.quest.stream
 import scala.concurrent.{ Await, Future }
 
 import akka.actor.ActorSystem
-import akka.stream.{ ActorMaterializer, ClosedShape, UniformFanInShape }
-import akka.stream.scaladsl.{ GraphDSL, RunnableGraph, Sink, Source, ZipWith }
+import akka.stream._
+import akka.stream.scaladsl.{ Broadcast, Flow, GraphDSL, RunnableGraph, Sink, Source, Zip, ZipWith }
 import scala.concurrent.duration._
+
+// Source is a partial graph with exactly one output, that is it returns a SourceShape.
+// Sink is a partial graph with exactly one input, that is it returns a SinkShape.
+// Flow is a partial graph with exactly one input and exactly one output, that is it returns a FlowShape.
 
 object PartialGraphMain extends App {
   implicit val system: ActorSystem = ActorSystem()
@@ -27,7 +31,7 @@ object PartialGraphMain extends App {
   val resultSink = Sink.head[Int]
 
   // GraphDSL is not able to provide compile time type-safety about whether or not all elements have been properly connected—this validation is performed as a runtime check during the graph's instantiation.
-//A partial graph also verifies that all ports are either connected or part of the returned Shape.
+  //A partial graph also verifies that all ports are either connected or part of the returned Shape.
   val g = RunnableGraph.fromGraph(GraphDSL.create(resultSink) { implicit b =>
     sink =>
       import GraphDSL.Implicits._
@@ -42,6 +46,47 @@ object PartialGraphMain extends App {
 
   val max: Future[Int] = g.run()
   println(Await.result(max, 200.millis))
+
+
+  val pairs = Source.fromGraph(GraphDSL.create() { implicit b =>
+    import GraphDSL.Implicits._
+    val zip = b.add(Zip[Int, Int]())
+
+    def ints = Source.fromIterator(() => Iterator.from(1))
+
+
+    // connect the graph
+    ints.filter(_ % 2 != 0) ~> zip.in0
+    ints.filter(_ % 2 == 0) ~> zip.in1
+
+    // expose port
+    SourceShape(zip.out)
+  })
+
+  val firstPair: Future[(Int, Int)] = pairs.runWith(Sink.head)
+
+  println(s"firstPair = ${Await.result(firstPair, 200.millis)}")
+
+
+  val pairUpWithToString =
+    Flow.fromGraph(GraphDSL.create() { implicit b =>
+      import GraphDSL.Implicits._
+
+      // prepare graph elements
+      val bcast = b.add(Broadcast[Int](2))
+      val zip = b.add(Zip[Int, String]())
+
+      // connect the graph
+      bcast.out(0).map { x => println(x); identity(x) } ~> zip.in0
+      bcast.out(1).map { x => println(x); x.toString } ~> zip.in1
+
+      // expose ports
+      FlowShape(bcast.in, zip.out)
+    })
+
+  val (_, f) = pairUpWithToString.runWith(Source(List(1, 2, 3)), Sink.last)
+  // TODO: fix it
+  println(s"f = ${Await.result(f, 200.millis)}")
 
   Thread.sleep(1000)
   system.terminate()
