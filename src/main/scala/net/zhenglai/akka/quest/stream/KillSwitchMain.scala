e5c81655742a2b9c234553f0f125bf07f431b587
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2017-02-05 11:53:20 +0800

    KillSwitch#shutdown

---
+++ src/main/scala/net/zhenglai/akka/quest/stream/KillSwitchMain.scala
@@ -0,0 +1,33 @@
+package net.zhenglai.akka.quest.stream
+
+import scala.concurrent.Await
+
+import akka.actor.ActorSystem
+import akka.stream.{ ActorMaterializer, DelayOverflowStrategy, KillSwitches }
+import akka.stream.scaladsl.{ Keep, Sink, Source }
+import scala.concurrent.duration._
+
+object KillSwitchMain extends App {
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val mat: ActorMaterializer = ActorMaterializer()
+  implicit val ec = system.dispatcher
+
+  val countingSrc = Source(Stream.from(1))
+    .delay(1 second, DelayOverflowStrategy.backpressure)
+  val lastSink = Sink.last[Int]
+
+  // UniqueKillSwitch allows to control the completion of
+  // one materialized Graph of FlowShape
+  val (killSwitch, last) = countingSrc
+    .viaMat(KillSwitches.single)(Keep.right)
+    .toMat(lastSink)(Keep.both)
+    .run()
+
+  Thread.sleep(2000)
+
+  killSwitch.shutdown()
+
+  println("last: " + Await.result(last, 1 second))
+
+  system.terminate()
+}
